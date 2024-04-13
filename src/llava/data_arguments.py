c4c4c9a94770bcb84c4eeb1a52386c0525d46f47
Kuleen Sasse <ksasse@uab.edu>
2024-04-12 20:54:58 -0500

    Added data arguments llava

---
+++ src/llava/data_arguments.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass, field
+
+from typing import Optional
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None,
+                           metadata={"help": "Path to the training data."})
+    lazy_preprocess: bool = False
+    is_multimodal: bool = False
+    image_folder: Optional[str] = field(default=None)
+    image_aspect_ratio: str = 'square'
