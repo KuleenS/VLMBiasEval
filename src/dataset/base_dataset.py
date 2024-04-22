70cef970c24092da26a3557571713274f708b650
Kuleen Sasse <ksasse@uab.edu>
2024-04-22 11:32:50 -0500

    Created base dataset for benchmark

---
+++ src/dataset/base_dataset.py
@@ -0,0 +1,16 @@
+from abc import ABC
+
+class Dataset(ABC):
+
+    def __init__(self, input_folder: str, output_folder: str) -> None:
+        super().__init__()
+
+        self.input_folder = input_folder
+
+        self.output_folder = output_folder
+    
+    def create_zero_shot_dataset(self) -> None:
+        pass
+        
+    def create_finetuning_dataset(self) -> None:
+        pass
