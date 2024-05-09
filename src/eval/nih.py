303438401dcd23521babed8494d7b7ebb50dbd8b
Kuleen Sasse <cometku@gmail.com>
2024-05-08 20:26:09 -0500

    nih eval

---
+++ src/eval/nih.py
@@ -0,0 +1,29 @@
+from typing import Dict, List, Any
+
+from src.eval.evaluate_dataset import BaseEvaluateDataset
+
+class NIHEval(BaseEvaluateDataset):
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.label_map = {"yes": 1, "no": 0}
+    
+    def evaluate_dataset_binary(self, data: List[Dict[str, Any]]) -> Dict[str, float]:
+
+        label_map_inverse = {1 : "yes", 0: "no"}
+
+        for i in range(len(data)):
+
+            data[i]["label"] = label_map_inverse[data[i]["label"]]
+
+        return super().evaluate_dataset_binary(data)
+
+
+
+
+        
+
+
+
+
