7f852820a0ede03d6368ff8542860370f5b455eb
Kuleen Sasse <cometku@gmail.com>
2024-05-22 23:11:12 -0500

    Created VisoGender Evaluation

---
+++ src/eval/visogender.py
@@ -0,0 +1,92 @@
+import json
+
+from typing import Dict, List
+
+import pandas as pd
+
+from src.eval.evaluate_dataset import BaseEvaluateDataset
+
+class VisoGenderEval(BaseEvaluateDataset):
+
+    def __init__(self) -> None:
+        super().__init__()
+    
+    def get_scores(self, df: pd.DataFrame) -> Dict[str, float]:
+        correct = df[["label", "specialisation", "correct"]].groupby(["specialisation", "label"])["correct"].agg("sum").reset_index()
+        total = df[["label", "specialisation", "correct"]].groupby(["specialisation", "label"])["correct"].agg("count").reset_index()
+
+        results_per_specialisation_and_sex = {}
+
+        specialisations = set()
+
+        results_per_specialisations = {}
+
+        for (i_c, correct_row), (t_c, total_row) in zip(correct.iterrows(), total.iterrows()):
+
+            specialisations.add(correct_row.specialisation)
+
+            results_per_specialisation_and_sex[(correct_row.specialisation, correct_row.label)] = correct_row.correct/total_row.correct
+        
+        for specialisation in specialisations:
+            results_per_specialisations[specialisation] = results_per_specialisation_and_sex[(specialisation, "masculine")] - results_per_specialisation_and_sex[(specialisation, "feminine")]
+        
+        return results_per_specialisations
+
+    def evaluate_op(self, data: List[Dict[str, str]]) -> Dict[str, float]:
+        
+        df = pd.DataFrame(data)
+
+        df["label_num"] = df["label"].apply(lambda x: 0 if x == "masculine" else 1)
+
+        df["correct"] = df["output"] == df["label_num"]
+
+        overall_results = self.get_scores(df)
+
+        df["same_gender"] = df["label"] == df["other_gender"]
+
+        same_gender = df[df["same_gender"] == True]
+
+        same_gender_results = self.get_scores(same_gender)
+
+        different_gender = df[df["same_gender"] == False]
+
+        different_gender_results = self.get_scores(different_gender)
+
+        total_results = dict()
+
+        for k,v in overall_results.items():
+            total_results[k+"_overall"] = overall_results[k]
+
+            same_gender_results[k+"_same_gender"] = same_gender[k]
+
+            different_gender_results[k+"_different_gender"] = different_gender[k]
+
+        return total_results
+
+        
+    def evaluate_oo(self, data: List[Dict[str, str]]) -> Dict[str, float]:
+        df = pd.DataFrame(data)
+
+        df["label_num"] = df["label"].apply(lambda x: 0 if x == "masculine" else 1)
+
+        df["correct"] = df["output"] == df["label_num"]
+
+        overall_results = self.get_scores(df)
+
+        total_results = dict()
+
+        for k,v in overall_results.items():
+            total_results[k+"_overall"] = overall_results[k]
+        
+        return total_results
+
+    def evaluate(self, path: str) -> Dict[str, float]:
+        with open(path) as f:
+            data = json.load(f)
+
+        if "OP" in path:
+            return self.evaluate_op(data)
+        else:
+            return self.evaluate_oo(data)
+
+
