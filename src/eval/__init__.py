258be9cf40745051e0ac9373c382320a7c661517
Kuleen Sasse <cometku@gmail.com>
2024-05-23 17:20:14 -0500

    added init eval to make easy inputs

---
+++ src/eval/__init__.py
@@ -0,0 +1,12 @@
+from src.eval.evaluate_dataset import BaseEvaluateDataset
+from src.eval.celeba import CelebAEval
+from src.eval.chexpert import CheXpertEval
+from src.eval.mimic import MIMICEval
+from src.eval.nih import NIHCXREval
+from src.eval.padchest import PadChestEval
+from src.eval.pata import PATAEval
+from src.eval.utkface import UTKFaceEval
+from src.eval.vindr import VinDREval
+from src.eval.visogender import VisoGenderEval
+from src.eval.vlstereo import VLStereoEval
+
