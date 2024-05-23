34c3b623643305a3501aa279c1316cabeea72f7d
Kuleen Sasse <cometku@gmail.com>
2024-05-22 22:31:54 -0500

    Updated model vqa to batching

--- src/scripts/model_vqa.py
+++ src/scripts/model_vqa.py
@@ -4,26 +4,29 @@ import os
 import json
 from tqdm import tqdm
 
-from LLaVA.llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN
-from LLaVA.llava.conversation import conv_templates
-from LLaVA.llava.model.builder import load_pretrained_model
-from LLaVA.llava.utils import disable_torch_init
-from LLaVA.llava.mm_utils import tokenizer_image_token, process_images, get_model_name_from_path
+from transformers import LlavaNextProcessor, LlavaNextForConditionalGeneration, AutoTokenizer
 
 import PIL
+
 from PIL import Image
 
-def batch(iterable, n=1):
+def batch_iterable(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
 
 def eval_model(args):
-    # Model
-    disable_torch_init()
-    model_path = os.path.expanduser(args.model_path)
-    model_name = get_model_name_from_path(model_path)
-    tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, args.model_base, model_name)
+    model_name = args.model_path
+
+    processor = LlavaNextProcessor.from_pretrained(model_name)
+
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    tokenizer.padding_side = "left"
+
+    tokenizer.pad_token_id = tokenizer.eos_token_id
+
+    model = LlavaNextForConditionalGeneration.from_pretrained(model_name, torch_dtype=torch.float16, low_cpu_mem_usage=True).to("cuda")
 
     question_files = [os.path.join(args.question_folder, x) for x in os.listdir(args.question_folder)]
 
@@ -38,77 +41,57 @@ def eval_model(args):
 
         model_outputs = []
 
-        questions_batched = batch(questions)
+        questions_batched = batch_iterable(questions, args.batch_size)
 
         for batch in tqdm(questions_batched):
             image_files = [x["image"] for x in batch]
+
             qs = [x["prompt"] for x in batch]
-            cur_prompts = qs
-            
-            for i in range(len(qs)):
-
-                if model.config.mm_use_im_start_end:
-                    qs[i] = DEFAULT_IM_START_TOKEN + DEFAULT_IMAGE_TOKEN + DEFAULT_IM_END_TOKEN + '\n' + qs[i]
-                else:
-                    qs[i] = DEFAULT_IMAGE_TOKEN + '\n' + qs[i]
-            
-            prompts = []
-
-            for i in range(len(qs)):
-
-                conv = conv_templates[args.conv_mode].copy()
-                conv.append_message(conv.roles[0], qs[i])
-                conv.append_message(conv.roles[1], None)
-                prompt = conv.get_prompt()
-
-                prompts.append(prompt)
-
-            input_ids = tokenizer_image_token(prompts, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').cuda()
 
             images = []
 
-            for image_file in image_files:
-                with Image.open(image_file) as img:
-                    image = img.convert('RGB')
-                    image.append(images)
+            prompts = []
+
+            for image_file, q in zip(image_files, qs):
+                try:
+                    images.append(Image.open(image_file))
+                    prompts.append("[INST] <image>\n"+ q +" [/INST]")
+                except PIL.UnidentifiedImageError:
+                    continue
             
-            image_tensor = process_images(images, image_processor, model.config)
+            if len(images) != 0 and len(prompts) != 0:
 
-            with torch.inference_mode():
-                output = model.generate(
-                    input_ids,
-                    images=image_tensor.half().cuda(),
-                    image_sizes=[image.size],
-                    do_sample=False,
-                    temperature=args.temperature,
-                    top_p=args.top_p,
-                    num_beams=args.num_beams,
-                    # no_repeat_ngram_size=3,
-                    output_scores=True,
-                    return_dict_in_generate=True,
-                    max_new_tokens=1,
-                    use_cache=True)
-            
-            g = output['scores'][0]
+                inputs = processor(prompts, images=images, padding=True, return_tensors="pt").to("cuda:0")
 
-            preds = []
+                with torch.inference_mode():
+                    output = model.generate(**inputs,
+                            max_new_tokens=1,
+                            output_scores=True,
+                            return_dict_in_generate=True,
+                            do_sample=False,
+                            temperature=args.temperature,
+                            top_p=args.top_p,
+                            num_beams=args.num_beams,
+                            )
+                
+                g = output['scores'][0]
 
-            for i in g:
-                pred_options_logits = torch.stack([i[tokenizer.convert_tokens_to_ids(y_label)] for y_label in output_labels])
-                pred = pred_options_logits.argmax(dim=-1).item()
+                preds = []
 
-                preds.append(pred)
-            
-            for cur_prompt, pred, line in zip(batch, cur_prompts, preds):
-                model_outputs.append(line)
-        
-                line["prompt"] = cur_prompt
+                for i in g:
+                    pred_options_logits = torch.stack([i[tokenizer.convert_tokens_to_ids(y_label)] for y_label in output_labels])
+                    pred = pred_options_logits.argmax(dim=-1).item()
 
-                line["model_id"] = model_name
+                    preds.append(pred)
+                
+                for line, prompt, pred in zip(batch, prompts, preds):
+                    line["prompt"] = prompt
 
-                line["output"] = pred
+                    line["model_id"] = model_name
 
-                model_outputs.append(line)
+                    line["output"] = pred
+
+                    model_outputs.append(line)
         
         model_name_clean = args.model_path.replace("/", "-")
         
@@ -120,10 +103,8 @@ def eval_model(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--model-path", type=str, default="facebook/opt-350m")
-    parser.add_argument("--model-base", type=str, default=None)
     parser.add_argument("--question_folder", type=str)
     parser.add_argument("--output_folder", type=str)
-    parser.add_argument("--conv-mode", type=str, default="llava_v1")
     parser.add_argument("--batch_size", type=int, default=32)
     parser.add_argument("--temperature", type=float, default=0.0)
     parser.add_argument("--top_p", type=float, default=None)
