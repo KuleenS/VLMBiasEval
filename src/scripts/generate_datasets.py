33e99ab6339e5d1a38eb489c6da7449501a21b2b
Kuleen Sasse <cometku@gmail.com>
2024-05-27 21:59:46 -0500

    created clip generate datasets

--- src/scripts/generate_datasets.py
+++ src/scripts/generate_datasets.py
@@ -7,7 +7,7 @@ from typing import Dict
 from src.dataset import *
 
 
-def generate_zeroshot_dataset(dataset_name: str, input_folder: str, output_folder: str, mode: str):
+def generate_zeroshot_dataset(dataset_name: str, input_folder: str, output_folder: str, mode: str, type_of_dataset: str):
     dataset_map: Dict[str, BaseDataset] = {
         "celeba": CelebA,
         "chexpert": CheXpert,
@@ -23,7 +23,10 @@ def generate_zeroshot_dataset(dataset_name: str, input_folder: str, output_folde
 
     dataset: BaseDataset = dataset_map[dataset_name](input_folder, output_folder, mode)
 
-    dataset.create_zero_shot_dataset()
+    if type_of_dataset == "clip":
+        dataset.create_clip_dataset()
+    else:
+        dataset.create_llava_dataset()
 
 def main(args):
     with open(args.config, "r") as f:
@@ -32,12 +35,14 @@ def main(args):
     datasets_to_generate = data["datasets"]
 
     output_folder = data["output_folder"]
+
+    type_of_dataset = data["type_of_dataset"]
     
     for dataset_name in datasets_to_generate:
         modes = data[dataset_name]["modes"]
 
         for mode in modes:
-            generate_zeroshot_dataset(dataset_name, data[dataset_name]["input_folder"], output_folder, mode)
+            generate_zeroshot_dataset(dataset_name, data[dataset_name]["input_folder"], output_folder, mode, type_of_dataset)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
