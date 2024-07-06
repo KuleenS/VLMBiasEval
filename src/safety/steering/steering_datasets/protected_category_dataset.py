20f0c4343da5a9e04e95895aee39c24854153a5d
Kuleen Sasse <cometku@gmail.com>
2024-07-05 19:05:15 -0500

    added protected category dataset

---
+++ src/safety/steering/steering_datasets/protected_category_dataset.py
@@ -0,0 +1,20 @@
+import json
+
+from src.safety.steering.steering_datasets.steering_dataset import SteeringDataset
+
+
+class ProtectedCategoryDataset(SteeringDataset):
+
+    def __init__(self, input_dataset: str):
+        with open(input_dataset, "r") as f:
+            data = json.loads(f.read())
+
+        self.questions = data["data"]
+    
+    def __getitem__(self, index):
+
+        q = self.questions[index]["prompt"]
+
+        protected_category = self.questions[index]["protected_category"]
+
+        return q, protected_category, self.questions[index]["image"]
