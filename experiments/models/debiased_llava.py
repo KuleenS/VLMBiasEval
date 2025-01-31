1fbc6d84f756b78e0b79f12d85f230038769e2ce
Kuleen Sasse <cometku@gmail.com>
2025-01-31 00:32:19 -0600

    made debiased llava eval model to wrap a bunch of ugly code

---
+++ experiments/models/debiased_llava.py
@@ -0,0 +1,82 @@
+from typing import List
+
+import torch
+
+import PIL
+
+from PIL.Image import Image
+
+from transformers import AutoTokenizer, AutoProcessor, AutoModelForImageTextToText
+
+from unbiasae.debias.wrapper import InterventionWrapper
+
+from experiments.models.model import EvalModel
+
+class DeBiasedLLaVaEvalModel(EvalModel):
+
+    def __init__(self, model_name: str, include_image: bool):
+        super().__init__(model_name)
+
+        self.include_image = include_image
+
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+
+        self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+
+        self.processor = AutoProcessor.from_pretrained(self.model_name)
+
+        self.model = AutoModelForImageTextToText.from_pretrained(self.model_name, low_cpu_mem_usage=True).to("cuda")
+
+        self.processor.tokenizer.padding_side = "left"
+
+        self.processor.tokenizer.pad_token_id = self.processor.tokenizer.eos_token_id
+
+        self.wrapper = InterventionWrapper(self.model, self.processor, self.model_name, self.include_image, device="cuda:0")
+
+        self.model_params = None
+
+        self.module_and_hook_fn = None
+
+    def load_wrapper(self, sae_release: str, sae_id: int, sae_layer: str, feature_idx: int):
+        self.wrapper.load_sae(release=sae_release, sae_id=sae_id, layer_idx=sae_layer)
+
+        self.model_params =  {
+            "sae_release": sae_release,
+            "sae_id": sae_id,
+            "targ_layer": sae_layer,
+            "feature_idx": feature_idx,
+        }
+    
+    def load_intervention(self, intervention_type: str, scaling_factor: float):
+        self.module_and_hook_fn = self.wrapper.get_hook(intervention_type, self.model_params, scaling_factor)
+    
+    def _process_image_and_prompt(self, image_file: str, prompt: str):
+        try:
+            processed_image = Image.open(image_file).convert('RGB')
+
+        except PIL.UnidentifiedImageError:
+            processed_image =  None 
+    
+        processed_prompt = f"<image>\n{prompt}"
+
+        return processed_prompt, processed_image
+    
+    def _get_outputs(self, prompt, image, output_labels):
+        g = self.wrapper.generate(prompt, image, self.module_and_hook_fn)
+
+        preds = []
+
+        for i in g:
+            pred_options_logits = torch.stack([i[self.tokenizer.convert_tokens_to_ids(y_label)] for y_label in output_labels])
+            pred = pred_options_logits.argmax(dim=-1).item()
+
+            preds.append(pred)
+        
+        return preds
+
+    def predict(self, q: str, image_file: str, output_labels: List[str]):
+        prompt, image = self._process_image_and_prompt(q, image_file, self.include_image)
+
+        if image is not None:
+            
+            return self._get_outputs(prompt, image, output_labels)[0]
