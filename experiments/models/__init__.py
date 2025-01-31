175afc936acb78e69b9e589c520b925b11a4e847
Kuleen Sasse <cometku@gmail.com>
2025-01-31 00:32:38 -0600

    modularized

---
+++ experiments/models/__init__.py
@@ -0,0 +1,4 @@
+from experiments.models.clip import CLIPEvalModel
+from experiments.models.gemini import GeminiEvalModel
+from experiments.models.llava import LLaVaEvalModel
+from experiments.models.model import EvalModel
