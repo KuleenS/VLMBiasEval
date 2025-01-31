a33bba90d9a17793aa96e43edd789dbe9bf04db3
Kuleen Sasse <cometku@gmail.com>
2025-01-31 00:34:39 -0600

    cleaned up base dataset to make it use pathlib and typing

--- unbiasae/dataset/base_dataset.py
+++ unbiasae/dataset/base_dataset.py
@@ -1,22 +1,24 @@
 from abc import ABC
 
+from pathlib import Path
+
+from typing import List, Dict
+
 class BaseDataset(ABC):
 
-    def __init__(self, input_folder: str, output_folder: str) -> None:
+    def __init__(self, input_folder: Path) -> None:
         super().__init__()
 
         self.input_folder = input_folder
 
-        self.output_folder = output_folder
-    
-    def create_train_llava_dataset(self) -> None:
+    def create_train_llava_dataset(self) -> Dict[str, Dict[str, str] | List]:
         pass
     
-    def create_test_llava_dataset(self) -> None:
+    def create_test_llava_dataset(self) -> Dict[str, Dict[str, str] | List]:
         pass
 
-    def create_train_clip_dataset(self) -> None:
+    def create_train_clip_dataset(self) -> Dict[str, Dict[str, str] | List]:
         pass
         
-    def create_test_clip_dataset(self) -> None:
+    def create_test_clip_dataset(self) -> Dict[str, Dict[str, str] | List]:
         pass
